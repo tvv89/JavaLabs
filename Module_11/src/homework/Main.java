6ce6e8c273c30ccb9635b6e0fe6a2e7c99004a54
tvv89 <tymchuk.volodymyr@gmail.com>
2017-01-23 17:14:23 +0200

    Module 11 task#1-4 with comments

--- Module_11/src/homework/Main.java
+++ Module_11/src/homework/Main.java
@@ -9,13 +9,20 @@ import java.util.Map;
  * Created by Volodymyr Tymchuk on 22.01.2017 for JavaLabs.
  */
 public class Main {
+    //global attribute file name
     static  final String fileName = "Module_11/src/homework/words.in";
 
         /*
         --------------------
         -------Task#1-------
         --------------------
+        You should create method which replace words in the File and returns String with replaced values
+        a) read file, save to string var
+        b) replace words
+        c) return new string
+        String replacer(Map<String, String> map)
         */
+
     public static String replacer(Map<String,String> map) throws IOException
     {
 
@@ -23,9 +30,11 @@ public class Main {
         StringBuilder sb;
         FileReader fileReader = null;
         try {
+           //a) read file,
             fileReader = new FileReader(fileName);
             br = new BufferedReader(fileReader);
             sb = new StringBuilder();
+            //save to string var
             String line = br.readLine();
             while (line != null) {
                 sb.append(line+ " ");
@@ -38,7 +47,7 @@ public class Main {
             if (fileReader!=null) fileReader.close();
         }
 
-
+        //save to string var
         String allWords;
         try {
             allWords = sb.toString();
@@ -47,27 +56,39 @@ public class Main {
         {
             return "Empty file";
         }
-
+       //b) replace words
         for (HashMap.Entry<String,String> e: map.entrySet()) {
             allWords = allWords.replace(e.getKey(),e.getValue());
         }
 
-
-    return allWords;
+       // c) return new string
+        return allWords;
     }
 
         /*
         --------------------
         -------Task#2-------
         --------------------
+        You should create method which replace words in the File and rewrite File content with new values
+        a) read file, save to string var
+        b) replace words
+        c) rewrite file with string
+        File fileContentReplacer(Map<String, String> map)
         */
     public static File fileContentReplacer(Map<String,String> map) throws IOException
     {
 
         FileWriter fileWriter = null;
         try {
+            /*
+            a) read file, save to string var
+            b) replace words
+
+            Use DRY
+             */
             String toWrite = replacer(map);
             fileWriter = new FileWriter(fileName);
+            //c) rewrite file with string
             fileWriter.write(toWrite);
         } catch (IOException e) {
             System.out.println("some problem with write file");
@@ -75,6 +96,7 @@ public class Main {
         finally {
             if (fileWriter != null) fileWriter.close();
         }
+        //return this file
         File res = new File(fileName);
         return res;
     }
@@ -83,12 +105,23 @@ public class Main {
     --------------------
     -------Task#3-------
     --------------------
+    You should create method which replace words in the File and write result to existing File content
+    a) read file, save to string var
+    b) replace words
+    c) add string to the existing file content
+    File fileContentMerger(Map<String, String> map)
     */
     public static File fileContentMerger(Map<String,String> map) throws IOException
     {
         FileWriter fileWriter = null;
         try {
+            /*
+            a) read file, save to string var
+            b) replace words
+             */
             String toWrite = replacer(map);
+            //c) add string to the existing file content
+            //true - inr FileWriter
             fileWriter = new FileWriter(fileName,true);
             fileWriter.write(System.lineSeparator());
             fileWriter.write(toWrite);
@@ -99,6 +132,7 @@ public class Main {
             if (fileWriter != null) fileWriter.close();
         }
         File res = new File(fileName);
+        //return this file
         return res;
     }
 
@@ -106,6 +140,12 @@ public class Main {
         --------------------
         -------Task#4-------
         --------------------
+        Check if file contains particular word. Pring 0 if no. Print number n which equals number of times it is contained in the file
+        a) read file, save to string var
+        b) calculate how many time the word occurs
+        c) print result
+        int checkWord(String word)
+        Write this task with usual try and try with resources (two versions)
         */
     public static int checkWord(String word) throws IOException
     {
@@ -113,6 +153,7 @@ public class Main {
         BufferedReader br;
         StringBuilder sb;
         FileReader fileReader = null;
+        //a) read file, save to string var
         try {
             fileReader = new FileReader(fileName);
             br = new BufferedReader(fileReader);
@@ -129,7 +170,6 @@ public class Main {
             if (fileReader!=null) fileReader.close();
         }
 
-
         String allWords;
         try {
             allWords = sb.toString();
@@ -139,11 +179,45 @@ public class Main {
             return 0;
         }
 
+        //b) calculate how many time the word occurs
         int count = 0;
         for (String i:allWords.split(" ")) {
             if (i.equals(word)) count++ ;
         }
 
+        return count;
+    }
+
+    //try with resources
+    public static int checkWord2(String word) throws IOException
+    {
+
+        BufferedReader br;
+        StringBuilder sb;
+        // a) read file, save to string var
+        try (FileReader fileReader = new FileReader(fileName)) {
+            br = new BufferedReader(fileReader);
+            sb = new StringBuilder();
+            String line = br.readLine();
+            while (line != null) {
+                sb.append(line + " ");
+                line = br.readLine();
+            }
+        }
+
+        String allWords;
+        try {
+            allWords = sb.toString();
+        }
+        catch (NullPointerException e)
+        {
+            return 0;
+        }
+        //b) calculate how many time the word occurs
+        int count = 0;
+        for (String i:allWords.split(" ")) {
+            if (i.equals(word)) count++ ;
+        }
 
         return count;
     }
@@ -179,8 +253,8 @@ public class Main {
         --------------------
         */
         String word = "hello";
-        System.out.println(word + " was in file " + checkWord(word) +" times");
-
+        System.out.println(word + " was in file " + checkWord(word) +" times (try-catch-finally)");
+        System.out.println(word + " was in file " + checkWord(word) +" times (try-with-recourse)");
 
 
 
