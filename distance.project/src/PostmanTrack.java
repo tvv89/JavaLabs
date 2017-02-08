2152e099ea28df325579ddbd23346287fc95df17
tvv89 <tymchuk.volodymyr@gmail.com>
2017-02-08 18:05:16 +0200

    distance.project

---
+++ distance.project/src/PostmanTrack.java
@@ -0,0 +1,15 @@
+/**
+ * Created by tvv89 on 08.02.2017 for JavaLabs.
+ */
+public class PostmanTrack {
+   /* public static TrackParameters[][] tracks = new TrackParameters[5][5];
+    public static void CreateMas() {
+        tracks[0][0] = new TrackParameters(0,0);
+        tracks[0][1] = new TrackParameters(1562,262);
+        tracks[0][2] = new TrackParameters(,0);
+        tracks[0][3] = new TrackParameters(0,0);
+        tracks[0][4] = new TrackParameters(0,0);
+
+
+    }*/
+}
