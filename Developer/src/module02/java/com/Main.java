7212f306f376f02348181912607bb09b38c2813e
tvv89 <tymchuk.volodymyr@gmail.com>
2017-03-27 00:25:16 +0300

    Developer: Module 02, full CRUD + Main

--- Developer/src/module02/java/com/Main.java
+++ Developer/src/module02/java/com/Main.java
@@ -1,10 +1,19 @@
 package module02.java.com;
 
+import module02.java.com.entity.Developer;
+import module02.java.com.repository.DeveloperRepo;
+
 import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws SQLException, ClassNotFoundException
     {
-
+        Developer developer1 = new Developer(new Long(1000),"Test1","Test1 surname", new Long(50000));
+        DeveloperRepo test = new DeveloperRepo(developer1);
+        test.create();
+        test.read();
+        developer1.setName("Test2");
+        test.update();
+        test.delete();
     }
 }
